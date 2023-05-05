64eb0bd3ded86d76ad225480171642ee1e54d1d9
Thomas Birney <tbirney@umd.edu>
2023-05-04 20:37:16 -0400

    Add install script for fiducials and readme docs

--- init_proj.sh
+++ init_proj.sh
@@ -6,3 +6,7 @@ catkin_make
 grep -qxF "source $ws_dir/devel/setup.bash" ~/.bashrc || echo "source $ws_dir/devel/setup.bash" >> ~/.bashrc
 grep -qxF "export TURTLEBOT3_MODEL=burger" ~/.bashrc || echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 source ~/.bashrc
+
+sudo apt update
+sudo apt-get install ros-noetic-fiducials
+sudo apt upgrade -y
--- readme.md
+++ readme.md
@@ -7,3 +7,7 @@ chmod 777 init_proj.sh
 
 ./init_proj.sh
 ```
+
+
+
+<a href="http://wiki.ros.org/fiducials">Fiducials</a> is a wrapper for <a href='http://wiki.ros.org/aruco_detect'>Aruco Detect</a> and <a href='http://wiki.ros.org/fiducial_slam'>Fiducial Slam</a> which handles processing all camera data.
